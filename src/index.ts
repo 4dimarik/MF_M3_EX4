6869a5cdb41fd9079ba353940071900dee4b299d
Dritriy Golyshev <4dimariK@gmail.com>
2023-06-27 22:19:46 +0300

    feat: add interface Comment & add check data function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import fetch from 'node-fetch';
 import { Response } from 'node-fetch';
 
-const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments1';
+interface Comment {
+  id: number;
+  postId: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments';
 
 const getData = (url: string): Promise<any> => {
   return fetch(url).then((response: Response) => response.json());
 };
 
-getData(COMMENTS_URL).then((data) => {});
+// функция проверяет что данные получены
+function isDataNotEmpty(obj: object): obj is Comment[] {
+  return Object.keys(obj).length > 0;
+}
+
+getData(COMMENTS_URL).then((data: object) => {});
 
 /**
  * ID: 1, Email: Eliseo...
